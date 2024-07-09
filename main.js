29a7d9ecb44006086f020bbd86c20143a310b225
Vũ Nga <vunga@Vus-MacBook-Pro.local>
2024-07-09 10:10:27 +0700

    server

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-var x = 10;
+const http = require("http");
 
-console.log(x);
+const hostname = "127.0.0.1";
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("HelloWorld");
+});
+
+server.listen(port, hostname, () => {
+  console.log(` Servver is Running at http://${hostname}:${port}`);
+});
